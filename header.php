d62acf276f5ec725eb21a13555be43b78778971d
applight <admin@app.lighting>
2021-11-27 03:46:37 -0500

    Update header.php

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <?php
 echo 
 '<header id="header">
-    <a class="logo" href="index.html">Matt Vaughan</a>
+    <a class="logo" href="index.php">Matt Vaughan</a>
     <nav>
         <a href="#menu">Menu</a>
     </nav>
